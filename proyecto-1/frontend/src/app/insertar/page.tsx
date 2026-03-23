e90380470111c5d32feeadb2547d54e94400497b
FabianSanchezD <fabiansanchezd@outlook.com>
2026-03-23 16:38:55 -0600

    feat: structure of pages, table, etc (mocked still)

---
+++ proyecto-1/frontend/src/app/insertar/page.tsx
@@ -0,0 +1,9 @@
+const InsertarEmpleados = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-bold text-4xl pt-10">Insertar Empleados</h1>
+    </div>
+  );
+};
+
+export default InsertarEmpleados;
